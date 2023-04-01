97f01f00e7320afc62af7d2d25aeb9ab467996dc
Leon <leon13713692135.@163.com>
2023-04-01 17:31:10 +0800

    idea first commit

---
+++ src/main/java/leon/git/GitTest.java
@@ -0,0 +1,11 @@
+package leon.git;
+
+/**
+ * @author leon
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
