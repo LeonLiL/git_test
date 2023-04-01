96c31f2c42c67bc8da398791edb550dcd220a001
Leon <leon13713692135.@163.com>
2023-04-01 22:30:29 +0800

    hot fix merge problem commit

--- src/main/java/leon/git/GitTest.java
+++ src/main/java/leon/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("Third Change hello git!");
         System.out.println("hot-fix change and get ready to merge");
+        System.out.println("hot-fix change and get ready to merge ,merge problems");
     }
 }
